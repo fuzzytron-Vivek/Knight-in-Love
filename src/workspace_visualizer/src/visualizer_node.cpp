d7356c289ad6a2ee7d01d849186fc91721fb4fa0
fuzzytron-Vivek <vivekdeexit@gmail.com>
2026-08-25 07:04:42 +0530

    Added tracker_ee and lookupTransform(tf2)

--- src/workspace_visualizer/src/visualizer_node.cpp
+++ src/workspace_visualizer/src/visualizer_node.cpp
@@ -2,10 +2,15 @@
 #include<sensor_msgs/msg/joint_state.hpp>
 #include<chrono>
 #include<functional>
+#include<tf2_ros/buffer.h>
+#include<tf2_ros/transform_listener.h>
+#include<tf2/exceptions.h>
+
+
 //for std::bind and placeholders
 class WorkspaceVisualizer:public rclcpp::Node{
   public:
-    WorkspaceVisualizer():Node("workspace_visualzer")
+    WorkspaceVisualizer():Node("workspace_visualzer"),buffer_(this->get_clock()),listener_(buffer_,this,false)
   {
     RCLCPP_INFO(this->get_logger(),"Workspace visualizer online.");
     jspub_ = this->create_publisher<sensor_msgs::msg::JointState>(
@@ -18,14 +23,19 @@ class WorkspaceVisualizer:public rclcpp::Node{
           this
           )
         );
-    //the placeholders correspond to the argument given to the callback,
+
+        //the placeholders correspond to the argument given to the callback,
     //so '_1' refers to the first argument being passed to callback
   }
       
   private:
+    rclcpp::Time start_time = this->get_clock()->now(); 
     rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr jspub_;
     rclcpp::TimerBase::SharedPtr timer_;
-//the const <datatype>::SharedPtr variable_name doesn't work over here 
+    tf2_ros::Buffer buffer_;
+    tf2_ros::TransformListener listener_;
+
+    //the const <datatype>::SharedPtr variable_name doesn't work over here 
 //we're creating a publisher callback , and hence we creat the object by ourselves
 //the const datatype also doesn't provide functionality since we will be using the publisher's own members 
 //
@@ -39,17 +49,42 @@ class WorkspaceVisualizer:public rclcpp::Node{
         "wrist_2_joint",
         "wrist_3_joint"
       };
+      rclcpp::Time now = this->get_clock()->now();
+      double t = (now - start_time).seconds();
       msg.position = {
-        0.5,6.9,4.0,2.0,0.0,0.0
+          2*sin(t),0.0,0.0,0.0,0.0,0.0
       };
       msg.header.stamp = this->get_clock()->now();
 
       jspub_->publish(msg);
 
-
+      track_eef();
       
 
     }
+    
+    void track_eef(){
+      const::std::string source_frame = "base_link";
+      const::std::string target_frame = "tool0";
+      geometry_msgs::msg::TransformStamped result;
+      try{
+      result = buffer_.lookupTransform(
+          source_frame,
+          target_frame,
+          tf2::TimePointZero
+          );
+      RCLCPP_INFO(this->get_logger(),
+          "EEF : %.3f , %.3f , %.3f",
+          result.transform.translation.x,
+          result.transform.translation.y,
+          result.transform.translation.z
+          );
+    }
+      catch(const tf2::TransformException &ex){
+        RCLCPP_WARN(this->get_logger(), "TF lookup failed %.s",ex.what());
+
+      }
+  }
 };
 int main(int argc , char * argv[]){
   rclcpp::init(argc , argv);
